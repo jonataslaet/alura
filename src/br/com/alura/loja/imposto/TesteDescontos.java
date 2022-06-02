5e0934448945bc95caf3adb08c62914848027be2
jonataslaet <jonataslaetprogramador@gmail.com>
2022-06-02 08:25:22 -0300

    introducao-design-patterns-java
    Manipulação de caso de teste sem a aplicação do Chain of Responsibility Pattern

--- src/br/com/alura/loja/imposto/TesteDescontos.java
+++ src/br/com/alura/loja/imposto/TesteDescontos.java
@@ -9,7 +9,12 @@ public class TesteDescontos {
 	
 	public static void main(String[] args) {
 		CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
-		System.out.println(calculadoraDeDescontos.calcular(new Orcamento(new BigDecimal("200"), 5)));
+		
+		Orcamento primeiro = new Orcamento(new BigDecimal("200"), 5);
+		Orcamento segundo = new Orcamento(new BigDecimal("501"), 5);
+		
+		System.out.println(calculadoraDeDescontos.calcular(primeiro));
+		System.out.println(calculadoraDeDescontos.calcular(segundo));
 	}
 
 }
