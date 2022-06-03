5d83f2bc088ecec9bc5729e6b9a08a64d9a4da3c
jonataslaet <jonataslaetprogramador@gmail.com>
2022-06-02 23:48:09 -0300

    introducao-design-patterns-java
    Com aplicação do Chain of Responsibility Pattern

---
+++ src/br/com/alura/loja/desconto/DescontoParaOrcamentoComMaisDeCincoItens.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja.desconto;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class DescontoParaOrcamentoComMaisDeCincoItens extends Desconto {
+
+	public DescontoParaOrcamentoComMaisDeCincoItens(Desconto desconto) {
+		super(desconto);
+	}
+
+	@Override
+	public BigDecimal calcular(Orcamento orcamento) {
+		if (orcamento.getQuantidadeItens() > 5) {
+			return orcamento.getValor().multiply(new BigDecimal("0.1"));
+		}
+		return proximo.calcular(orcamento);
+	}
+
+}
