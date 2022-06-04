82e8f7eeacaec678b54e24254bdb40ee1d0cbc53
jonataslaet <jonataslaetprogramador@gmail.com>
2022-06-04 16:49:24 -0300

    introducao-design-patterns-java
    Sem aplicação do State Pattern

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,10 +6,34 @@ public class Orcamento {
 
 	private BigDecimal valor;
 	private int quantidadeItens;
+	private String situacao;
 	
 	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		this.valor = valor;
 		this.quantidadeItens = quantidadeItens;
+		this.situacao = "EM ANALISE";
+	}
+	
+	public void aplicarDescontoExstra() {
+		BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+		if (situacao.equals("EM ANALISE")) {
+			valorDoDescontoExtra = new BigDecimal("0.05");
+		} else if (situacao.equals("APROVADO")) {
+			valorDoDescontoExtra = new BigDecimal("0.02");
+		}
+		this.valor = this.valor.subtract(valorDoDescontoExtra);
+	}
+	
+	public void aprovar() {
+		this.situacao = "APROVADO";
+	}
+
+	public void reprovar() {
+		this.situacao = "REPROVADO";
+	}
+	
+	public void finalizar() {
+		this.situacao = "FINALIZADO";
 	}
 	
 	public BigDecimal getValor() {
