20b23f5c2a016249d585e8370209ca7c9af5eb75
jonataslaet <jonataslaetprogramador@gmail.com>
2022-06-16 22:51:45 -0300

    solid-orientacao-objetos-java
    Acrescentada mais uma validação no reajuste

--- solid-orientacao-objetos-java/src/br/com/alura/rh/service/ReajusteService.java
+++ solid-orientacao-objetos-java/src/br/com/alura/rh/service/ReajusteService.java
@@ -2,6 +2,8 @@ package br.com.alura.rh.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
@@ -13,6 +15,11 @@ public class ReajusteService {
 		BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
 		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+		}
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		Long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, LocalDate.now());
+		if (mesesDesdeUltimoReajuste < 6) {
+			throw new ValidacaoException("A periodicidade entre reajuste deve ser de no mínimo 6 meses!");
 		}
 		funcionario.atualizarSalario(salario);
 	}
